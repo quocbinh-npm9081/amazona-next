2d50185c170400179e44264552d71d16889ccc31
louis <quocbinhgithub@gmail.com>
2023-04-20 20:17:58 +0700

    update database for Local

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,16 @@
-import axios from 'axios';
-import { useContext } from 'react';
-import { toast } from 'react-toastify';
-import Layout from '../components/Layout';
-import ProductItem from '../components/ProductItem';
-import Product from '../models/Product';
-import db from '../utils/db';
-import { Store } from '../utils/Store';
-import { Carousel } from 'react-responsive-carousel';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import Link from 'next/link';
-
+import axios from "axios";
+import { useContext } from "react";
+import { toast } from "react-toastify";
+import Layout from "../components/Layout";
+import ProductItem from "../components/ProductItem";
+import Product from "../models/Product";
+import db from "../utils/db";
+import { Store } from "../utils/Store";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import Link from "next/link";
+import CarouselBanner from "../components/CarouselBanner";
+import dataFake from "../utils/data";
 export default function Home({ products, featuredProducts }) {
   const { state, dispatch } = useContext(Store);
   const { cart } = state;
@@ -20,16 +21,16 @@ export default function Home({ products, featuredProducts }) {
     const { data } = await axios.get(`/api/products/${product._id}`);
 
     if (data.countInStock < quantity) {
-      return toast.error('Sorry. Product is out of stock');
+      return toast.error("Sorry. Product is out of stock");
     }
-    dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } });
+    dispatch({ type: "CART_ADD_ITEM", payload: { ...product, quantity } });
 
-    toast.success('Product added to the cart');
+    toast.success("Product added to the cart");
   };
 
   return (
     <Layout title="Home Page">
-      <Carousel showThumbs={false} autoPlay>
+      {/* <Carousel showThumbs={false} autoPlay>
         {featuredProducts.map((product) => (
           <div key={product._id}>
             <Link href={`/product/${product.slug}`} passHref className="flex">
@@ -37,7 +38,9 @@ export default function Home({ products, featuredProducts }) {
             </Link>
           </div>
         ))}
-      </Carousel>
+      </Carousel> */}
+      <CarouselBanner banners={dataFake.banners} />
+
       <h2 className="h2 my-4">Latest Products</h2>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
         {products.map((product) => (
